6e86308a56acf7d6ba005f05ab66e08b5ed6591b
orioldedios <orioldedios@gmail.com>
2019-03-26 09:58:55 +0100

    lalal

--- Pr_1/Práctica.py
+++ Pr_1/Práctica.py
@@ -3,26 +3,50 @@ import cv2
 
 if __name__ == '__main__':
 
-    img_I = cv2.imread('img1.png', cv2.IMREAD_GRAYSCALE)
-    img_T = cv2.imread('t1-img1.png', cv2.IMREAD_GRAYSCALE)
+    INPUT = input("Input image: ")
+    target = input("Target image: ")
+    threshold = float(input("Threshold: "))
+
+    img_I_color = np.float64(cv2.imread(INPUT, -1))
+    img_T_color = np.float64(cv2.imread(target, -1))
+
+    img_I = np.float64(cv2.imread(INPUT, cv2.IMREAD_GRAYSCALE))
+    img_T = np.float64(cv2.imread(target, cv2.IMREAD_GRAYSCALE))
+
+    imgT_w = img_T.shape[0]
+    imgT_h = img_T.shape[1]
+
+    imgI_w = img_I.shape[0]
+    imgI_h = img_I.shape[1]
 
     imgFinal_w = img_I.shape[0] - img_T.shape[0] + 1
     imgFinal_h = img_I.shape[1] - img_T.shape[1] + 1
     img_FINAL = np.zeros((imgFinal_w, imgFinal_h))
 
     bigestError = 0
+    lowestError = 100000000
 
     for y in range(0, imgFinal_h):
         for x in range(0, imgFinal_w):
             iterativeRes = 0
             for j in range(0, img_T.shape[1]):
-                for i in range(0, img_I.shape[0]):
+                for i in range(0, img_T.shape[0]):
                     iterativeRes += (img_T[i, j] - img_I[x + i, y + j]) ** 2
             if iterativeRes > bigestError:
                 bigestError = iterativeRes
+            if iterativeRes < lowestError:
+                lowestError = iterativeRes
+            if iterativeRes < threshold:
+                cv2.rectangle(img_I_color, (x - 1, y - 1), (x + imgT_w, y + imgT_h), (0.0, 255.0, 0.0))
             img_FINAL[x, y] = iterativeRes
 
-    img_FINAL[:] / bigestError * 255.0
+    if lowestError / bigestError < threshold:
+        print("TARGET FOUND")
 
-    cv2.imshow('DAMN',img_FINAL)
+    img_FINAL[:] = (img_FINAL[:] / bigestError * 255.0)
+
+
+    cv2.imshow('Target Image', np.uint8(img_T_color))
+    cv2.imshow('Input Image', np.uint8(img_I_color))
+    cv2.imshow('Maching Map', np.uint8(img_FINAL))
     cv2.waitKey(0)
